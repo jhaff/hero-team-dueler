1a06de88cab88a68d1da79e783f1b955a7e4d234
jhaff <jacobhaff2@gmail.com>
2018-09-13 10:32:53 -0700

    initial commit, created Hero class with a few functions

---
+++ superheroes.py
@@ -0,0 +1,11 @@
+class Hero:
+
+    def init(self, name): # Initialize starting values
+
+    def add_ability(self, ability):
+        # Add ability to abilities list
+
+    def attack(self):
+        # Run attack() on every ability hero has
+
+        
