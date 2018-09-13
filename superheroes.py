22cec9a7aef99e23f3ce25528a4db3c797f453a0
jhaff <jacobhaff2@gmail.com>
2018-09-13 13:47:01 -0700

    wrote attack functions for Hero and Ability classes

--- superheroes.py
+++ superheroes.py
@@ -1,43 +1,50 @@
 import random
 
-name = "main";
 
 class Ability:
 
-    name = "";
-    attack_strength = 0;
+    name = ""
+    attack_strength = 0
 
     def __init__(self, name, attack_strength):
-        self.name = name;
-        self.attack_strength = attack_strength;
-        print("example");
+        self.name = name
+        self.attack_strength = attack_strength
+        print("example")
 
     def attack(self):
+
+        lower_value = self.attack_strength // 2
+
+        return random.randint(lower_value, self.attack_strength)
+
         # Return attack value
-        print("example");
 
     def update_attack(self, attack_strength):
             # Update attack value
-        print("example");
+        print("example")
+
+
 class Hero:
 
-    name = ""
-    abilities = Ability();
+    # name = ""
+    # abilities = list();
 
     def __init__(self, name): # Initialize starting values
-        self.name = name;
+        self.name = name
         self.abilities = list()
     def add_ability(self, ability): # Add ability to abilities list
-        abilities.append(ability);
-        print("example");
+        self.abilities.append(ability)
 
     def attack(self): # Run attack() on every ability hero has
-        for ability in abilities:
-            ability.attack();
-        print("example");
+        total = 0
+
+        for ability in self.abilities:
+            total += ability.attack()
+
+        return total
 
 
-if name == "main":
+if __name__ == "__main__":
     hero = Hero("Wonder Woman")
     # print(hero.attack())
     ability = Ability("Divine Speed", 300)
